72e90f9f0bfcbf2fa5ba36345e68033bd771cb4a
dojo-json <jfranz@codingdojo.com>
2019-05-09 18:11:04 -0700

    client login

--- books/src/app/app-routing.module.ts
+++ books/src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import * as fromBooks from './books';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'books',
-    pathMatch: 'full',
+    component: HomeComponent,
   },
   {
     path: 'books',
--- books/src/app/app.module.ts
+++ books/src/app/app.module.ts
@@ -2,16 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { CookieModule } from 'ngx-cookie';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import * as fromBooks from './books';
 import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './home/login/login.component';
+import { RegisterComponent } from './home/register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, ...fromBooks.components, NavComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    ...fromBooks.components,
+    NavComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    CookieModule.forRoot(),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
